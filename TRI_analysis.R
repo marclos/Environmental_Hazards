5e614b220dd461367bd11362a94a59296e503346
marclos <marc.loshuertos@pomona.edu>
2016-08-03 23:12:52 -0700

    Import header=F csv file

--- TRI_analysis.R
+++ TRI_analysis.R
@@ -2,4 +2,8 @@
 
 lacounty = "/home/CAMPUS/mwl04747/github/Environmental_Hazards/Data/LA_county_2015_chem_release2.csv"
 
-import=read.csv(lacounty); names(import)
+import=read.csv(lacounty, header=F); names(import)
+names(import)=c("Row #", "Chemical", "On-Site", "Off-Site", "Total")
+
+head(import)
+
