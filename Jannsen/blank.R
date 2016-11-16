801e241abd027fadb5c6b6468f74c56c009561ff
Sophia Janssen <smjx2015@mymail.pomona.edu>
2016-11-16 13:49:36 -0800

    FIRST ROUND

--- Jannsen/blank.R
+++ Jannsen/blank.R
@@ -1,1 +1,17 @@
-# blank
+file to help find stuff on Toxic Release Inventory Program
+
+https://www.epa.gov/sites/production/files/2015-06/documents/factors_to_consider_6.15.15_final.pdf
+file="/home/CAMPUS/smjx2015/Environmental_Hazards/Jannsen/TESORO LOS ANGELES REFINERY-CARSON OPERATIONS.csv"
+Civilian Labor Force 2013: 244,912;
+
+Unemployed: 11.5%;
+
+Per capita personal income 2013: $25,933
+
+import=read.csv(file)
+plot(Total.on..and.off.site.releases~Year, data=import)
+str(import)
+plot(Total.on..and.off.site.releases~Year, data=import[import$Chemical=="LEAD",])
+plot(Total.on..and.off.site.releases~Year, data=import[import$Chemical=="MERCURY",])
+plot(Total.on..and.off.site.releases~Year, data=import[import$Chemical=="CHROMIUM",])                                                  
+plot(Total.on..and.off.site.releases~Year, data=import[import$Chemical=="BENZENE",])
