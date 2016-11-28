2cef16d0153acb56e01f82d46d3047d32309097a
vesj2015 <vesj2015@mymail.pomona.edu>
2016-11-28 08:23:22 -0800

    mostly map

--- Sanchez-Jimenez/blank.R
+++ Sanchez-Jimenez/blank.R
@@ -14,18 +14,17 @@ Toluene <- subset(import, Chemical=="ETHYLENE")
 
 plot(Fugitive.Air  ~ Year, data=Toluene, las=1, pch=19, ylab="Fugitive Air Emissions (lbs)", xlim=c(1985, 2014))
 
-plot(Stack.Air  ~ Year, data=Toluene, las=1, pch=19, ylab="Fugitive Air Emissions (lbs)", xlim=c(1985, 2014))
+plot(Stack.Air  ~ Year, data=Toluene, las=1, pch=19, ylab="Stack Air Emissions (lbs)", xlim=c(1985, 2014))
 
-plot(Total.Air  ~ Year, data=Toluene, las=1, pch=19, ylab="Fugitive Air Emissions (lbs)", xlim=c(1985, 2014))
+plot(Total.Air  ~ Year, data=Toluene, las=1, pch=19, ylab="Total Air Emissions (lbs)", xlim=c(1985, 2014))
 
-plot(Surface.Water.Discharge ~ Year, data=Toluene, las=1, pch=19, ylab="Fugitive Air Emissions (lbs)", xlim=c(1985, 2014))
+plot(Surface.Water.Discharge ~ Year, data=Toluene, las=1, pch=19, ylab="Surface Water Discharge (lbs)", xlim=c(1985, 2014))
 
-plot(Underground.Injection ~ Year, data=Toluene, las=1, pch=19, ylab="Fugitive Air Emissions (lbs)", xlim=c(1985, 2014))
+plot(Underground.Injection ~ Year, data=Toluene, las=1, pch=19, ylab="Underground Water Injection (lbs)", xlim=c(1985, 2014))
 
-plot(Land.Disposal ~ Year, data=Toluene, las=1, pch=19, ylab="Fugitive Air Emissions (lbs)", xlim=c(1985, 2014))
+plot(Land.Disposal ~ Year, data=Toluene, las=1, pch=19, ylab="Land Disposal (lbs)", xlim=c(1985, 2014))
 
 plot(Total.on.site.release ~ Year, data=Toluene, las=1, pch=19, ylab="Fugitive Air Emissions (lbs)", xlim=c(1985, 2014))
+plot(Off.site.release ~ Year, data=Toluene, las=1, pch=19, ylab="Fugitive Air Emissions (lbs)", xlim=c(1985, 2014))
 
-
-Off.site.release
-
+#last one has problems, but no need?
