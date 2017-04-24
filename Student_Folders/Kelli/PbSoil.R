4b9d796b85c38cb5313767bca84e9d35d6d5118f
kak22013 <kak22013@mymail.pomona.edu>
2017-04-24 15:53:41 -0700

    prep for anova

--- Student_Folders/Kelli/PbSoil.R
+++ Student_Folders/Kelli/PbSoil.R
@@ -1,3 +1,7 @@
 PbSoil <- read.csv("/home/CAMPUS/kak22013/Environmental_Hazards/Student_Folders/Kelli/NewEA_PbSoil.csv")
-na.omit("NewEA_PbSoil.csv")
+str(PbSoil)
+PbSoilEdit <- na.omit(PbSoil)
+
+
+
 
